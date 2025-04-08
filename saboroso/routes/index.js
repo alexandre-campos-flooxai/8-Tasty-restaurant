eb9fa69e0a56b89c32aba5defed0378f1628780b
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-04-08 10:06:55 -0300

    feat: reserve configuration

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,6 +1,7 @@
 var conn = require("./../inc/db");
 var express = require("express");
 var menus = require("./../inc/menus");
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
@@ -33,19 +34,38 @@ router.get("/menus", function (req, res, next) {
   });
 });
 
+router.get("/reservations", function (req, res, next) {
+  reservations.render(req,res);
+
+});
+
+router.post("/reservations", function (req, res, next) {
+  if(!req.body.name){
+    reservations.render(req, res, "Digite o nome")
+  }else if(!req.body.email){
+    reservations.render(req, res, "Digite o e-mail")
+  }else if(!req.body.people){
+    reservations.render(req, res, "Digite o número de pessoas")
+  }else if(!req.body.date){
+    reservations.render(req, res, "Seleciona a data")
+  }else if(!req.body.time){
+    reservations.render(req, res, "Seleciona a hora")
+  }else{
+    reservations.save(req.body).then(results =>{
+      req.body = {}
+
+      reservations.render(req, res, null, "Reserva realizada com sucesso!")
+    }).catch(err=>{
+      reservations.render(req,res,err.message)
+    })
+  }
+});
+
 router.get("/services", function (req, res, next) {
   res.render("services", {
     title: "Serviços - Restaurante Saboroso!",
     background: "images/img_bg_1.jpg",
     h1: "É um prazer poder servir!",
-  });
-});
-
-router.get("/reservations", function (req, res, next) {
-  res.render("reservations", {
-    title: "Reservas - Restaurante Saboroso!",
-    background: "images/img_bg_2.jpg",
-    h1: "Reserve uma Mesa!",
   });
 });
 
