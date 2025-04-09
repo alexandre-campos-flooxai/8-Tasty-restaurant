0518fe44de8c89a4e82f42c5abc6bc99b2b05845
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-04-09 19:10:36 -0300

    feat: saving files to the bank

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var users = require("./../inc/users");
-var admin = require('./../inc/admin');
-var menus = require('./../inc/menus');
+var admin = require("./../inc/admin");
+var menus = require("./../inc/menus");
 var router = express.Router();
-
 
 router.use(function (req, res, next) {
   if (["/login"].indexOf(req.url) === -1 && !req.session.user) {
@@ -13,26 +12,32 @@ router.use(function (req, res, next) {
   }
 });
 
-router.use(function(req,res,next){
-  req.menus = admin.getMenus(req)
+router.use(function (req, res, next) {
+  req.menus = admin.getMenus(req);
 
-  next()
-})
+  next();
+});
 
-router.use('/logout',function (req, res, next) {
+router.use("/logout", function (req, res, next) {
   delete req.session.user;
 
   res.redirect("/admin/login");
 });
 
 router.get("/", function (req, res, next) {
-  admin.dashboard().then(data=>{
-    res.render("admin/index", admin.getParams(req, {
-      data
-    }));
-  }).catch(err =>{
-    console.error(err)
-  })
+  admin
+    .dashboard()
+    .then((data) => {
+      res.render(
+        "admin/index",
+        admin.getParams(req, {
+          data,
+        })
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 router.post("/login", function (req, res, next) {
@@ -66,22 +71,34 @@ router.get("/emails", function (req, res, next) {
 });
 
 router.get("/menus", function (req, res, next) {
-  menus.getMenus().then(data =>{
-    res.render("admin/menus", admin.getParams(req,{
-      data
-    }));
-  })
+  menus.getMenus().then((data) => {
+    res.render(
+      "admin/menus",
+      admin.getParams(req, {
+        data,
+      })
+    );
+  });
 });
 
-router.post('/menus', function(req,res,next){
-  res.send(req.body)
-})
-
+router.post("/menus", function (req, res, next) {
+  menus
+    .save(req.fields, req.files)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 router.get("/reservations", function (req, res, next) {
-  res.render("admin/reservations", admin.getParams(req, {
-    date:{}
-  }));
+  res.render(
+    "admin/reservations",
+    admin.getParams(req, {
+      date: {},
+    })
+  );
 });
 
 router.get("/users", function (req, res, next) {
