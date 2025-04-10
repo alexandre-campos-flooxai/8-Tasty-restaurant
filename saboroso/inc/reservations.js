abd338b2912caeccef9dc5e08483b10eb27f1cd7
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-04-10 11:45:55 -0300

    feat: creating administration reserve

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -1,40 +1,58 @@
 var conn = require("./db");
 
 module.exports = {
-  render(req, res, error,success) {
+  render(req, res, error, success) {
     res.render("reservations", {
       title: "Reservas - Restaurante Saboroso!",
       background: "images/img_bg_2.jpg",
       h1: "Reserve uma Mesa!",
       body: req.body,
       error,
-      success
+      success,
     });
   },
 
   save(fields) {
     return new Promise((resolve, reject) => {
-        let date = fields.date.split('/');
-        fields.date = `${date[2]}-${date[1]}-${date[0]}`
-      conn.query(`
+      if(fields.date.indexOf('/') > -1){
+        let date = fields.date.split("/");
+        fields.date = `${date[2]}-${date[1]}-${date[0]}`;
+      }
+  
+      let query, params = [
+          fields.name,
+          fields.email,
+          fields.people,
+          fields.date,
+          fields.time,
+        ];
+
+      if (parseInt(fields.id) > 0) {
+        query = `
+            UPDATE tb_reservations
+            SET
+              name = ?,
+              email = ?,
+              people = ?
+              date = ?,
+              time = ?
+            WHERE id = ?
+          `;
+        params.push(fields.id);
+      } else {
+        query = `
             INSERT INTO tb_reservations (name, email, people, date, time)    
             VALUES(?, ?, ?, ?, ?)
-        `,
-        [fields.name, 
-        fields.email, 
-        fields.people, 
-        fields.date, 
-        fields.time],
-        (err, results) => {
-            console.log(err)
+        `;
+      }
 
-            if(err){
-                reject(err)
-            }else{
-                resolve(results)
-            }
+      conn.query(query, params, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
         }
-      );
+      });
     });
   },
 };
