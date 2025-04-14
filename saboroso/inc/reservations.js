3ee5393b8b6b8eba9e0cb8a75aa044683e2ae835
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-04-14 20:05:20 -0300

    feat: aplication socket.in

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -1,6 +1,6 @@
 var conn = require("./db");
 const Pagination = require("./Pagination");
-var moment = require('moment');
+var moment = require("moment");
 
 module.exports = {
   render(req, res, error, success) {
@@ -16,12 +16,13 @@ module.exports = {
 
   save(fields) {
     return new Promise((resolve, reject) => {
-      if(fields.date.indexOf('/') > -1){
+      if (fields.date.indexOf("/") > -1) {
         let date = fields.date.split("/");
         fields.date = `${date[2]}-${date[1]}-${date[0]}`;
       }
-  
-      let query, params = [
+
+      let query,
+        params = [
           fields.name,
           fields.email,
           fields.people,
@@ -64,32 +65,29 @@ module.exports = {
     let dtend = req.query.end;
 
     return new Promise((resolve, reject) => {
-
-      if(!page) page = 1;
+      if (!page) page = 1;
 
       let params = [];
 
-      if(dtstart && dtend) params.push(dtstart,dtend);
-    
+      if (dtstart && dtend) params.push(dtstart, dtend);
+
       let pag = new Pagination(
-          `
+        `
             SELECT  SQL_CALC_FOUND_ROWS * 
             FROM tb_reservations
-            ${(dtstart && dtend) ? 'WHERE date BETWEEN ? AND ?' : ''}
+            ${dtstart && dtend ? "WHERE date BETWEEN ? AND ?" : ""}
             ORDER BY name LIMIT ?, ?
           `,
-          params
-       );
+        params
+      );
 
-      pag.getPage(page).then(data =>{
+      pag.getPage(page).then((data) => {
         resolve({
           data,
-          links:pag.getNavigation(req.query)
-        })
-      })
-     
+          links: pag.getNavigation(req.query),
+        });
+      });
     });
-    
   },
 
   delete(id) {
@@ -110,9 +108,10 @@ module.exports = {
     });
   },
 
-  chart(req){
-    return new Promise((resolve,reject)=>{
-      conn.query(`
+  chart(req) {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
             SELECT
                 CONCAT(YEAR(date), '-', MONTH(date)) AS date,
                 COUNT(*) AS total,
@@ -122,26 +121,46 @@ module.exports = {
                date BETWEEN ? AND ?
             GROUP BY CONCAT(YEAR(date), '-', MONTH(date))
             ORDER BY date DESC;
-        `,[
-          req.query.start,
-          req.query.end,
-        ],(err, results) => {
-          if(err){
-            reject(err)
-          }else{
+        `,
+        [req.query.start, req.query.end],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
             let months = [];
             let values = [];
 
-            results.forEach(row =>{
+            results.forEach((row) => {
               months.push(moment(row.date).format("MMM YYYY"));
-              values.push(row.total)
+              values.push(row.total);
             });
             resolve({
               months,
-              values
-            })
+              values,
+            });
           }
-        })
-    })
-  }
+        }
+      );
+    });
+  },
+  dashboard() {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
+            SELECT
+                (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
+                (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
+                (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
+                (SELECT COUNT(*) FROM tb_users) AS nrusers
+        `,
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results[0]);
+          }
+        }
+      );
+    });
+  },
 };
